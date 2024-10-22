b7f6fb460e421b8ca5a1b6dbef19d2ae99e39fdc
letalandroid <carlossoncra@gmail.com>
2024-10-22 18:08:41 -0500

    Recogiendo objetos

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -1,10 +1,28 @@
+using System.Collections;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("Player")) {
-            other.GetComponent<Player>().objPick = this.gameObject;
+            Player obj = other.GetComponent<Player>();
+            Animator anim = other.GetComponent<Animator>();
+
+            if (obj.objPick == null) {
+                obj.objPick = this.gameObject;
+                anim.SetBool("isRecoger", true);
+
+                StartCoroutine(changeState(anim));
+            } else {
+                Debug.Log("Ya tiene un objeto en la mano");
+            }
         }
+    }
+
+    private IEnumerator changeState(Animator anim)
+    {
+        yield return new WaitForSeconds(3);
+        anim.SetBool("isRecoger", false);
+        this.gameObject.SetActive(false);
     }
 }
